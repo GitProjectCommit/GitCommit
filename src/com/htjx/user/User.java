21c54f334ca279491b574280a63f143abd248435
GitProjectCommit <2690164089@qq.com>
2016-06-13 14:46:14 +0800

    1.0

--- src/com/htjx/user/User.java
+++ src/com/htjx/user/User.java
@@ -3,7 +3,9 @@ package com.htjx.user;
 public class User {
 	
 	public static void main(String[] args) {
-		System.out.println("ssss");
+//		System.out.println("ssss");
+		System.out.println("002");
+		
 	}
 
 }
